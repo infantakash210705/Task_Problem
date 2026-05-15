cfd8245c5a16401ddc664536be63efb340f6c15a
infantakash210705 <infantakash260@gmail.com>
2026-05-15 15:07:24 +0530

    up to the date updated

--- problem.java
+++ problem.java
@@ -45,14 +45,12 @@ import java.util.*;
 //     }
 // }
 
-
 ///////////// Problem -> 2
 // given a number from 1 to 9, find the next smallest palindrome strictly greater than the given number.
 // input:
 // 9 4 1 8 7 9 7 8 3 2 2
 // output:
 // 9 4 1 8 8 0 8 8 1 4 9
-
 
 ///////////// Problem -> 3
 // check whether a given number is a palindrome or not.
@@ -83,7 +81,6 @@ import java.util.*;
 //     }
 // }
 
-
 ///////////// Problem -> 4
 // one chocolate costs 1 rupee. Rohit has n rupees.
 // the shop offers 1 free chocolate for every 3 wrappers returned.
@@ -110,7 +107,6 @@ import java.util.*;
 //     }
 // }
 
-
 ///////////// Problem -> 5
 // in a farm, there are rabbits and chickens.
 // given total number of heads and legs, find the number of rabbits and chickens.
@@ -133,7 +129,6 @@ import java.util.*;
 //         System.out.println("chicken: " + chicken);
 //     }
 // }
-
 
 ///////////// Problem -> 6
 // given an array, find all the leaders in the array.
@@ -172,7 +167,6 @@ import java.util.*;
 //     }
 // }
 
-
 ///////////// Problem -> 7
 // given a sorted array, rearrange the elements in place to form a wave like array.
 // an array is said to be in wave form if:
@@ -204,7 +198,6 @@ import java.util.*;
 //         }
 //     }
 // }
-
 
 ///////////// Problem -> 8
 // given a non-negative number represented as an array of digits,
@@ -244,8 +237,6 @@ import java.util.*;
 //     }
 // }
 
-
-
 // Date: 30/04/2026
 ///////////// Problem -> 1
 // Given an array the task is to rearrange in alternative positive
@@ -265,7 +256,7 @@ import java.util.*;
 //         for(int i = 0; i < n; i++){
 //             arr[i] = sc.nextInt();
 //         }
-        
+
 //         int[] pos = new int[n];
 //         int[] neg = new int[n];
 //         int p = 0, q = 0;
@@ -298,9 +289,6 @@ import java.util.*;
 //         }
 //     }
 // }
-
-
-
 
 ///////////// Problem -> 2
 // Given an array find the element that appears
@@ -354,9 +342,6 @@ import java.util.*;
 //     }
 // }
 
-
-
-
 ///////////// Problem -> 3
 // Given an array at each operation we can select any one element
 // and increase rest of n-1 elements by 1. we have to make all element 
@@ -401,21 +386,18 @@ import java.util.*;
 //                     ind = i;
 //                 }
 //             }
-            
+
 //             for(int i = 0; i < n; i++){
 //                 if(i != ind){
 //                     arr[i] += 1;
 //                 }
 //             }
 //             count++;
-            
+
 //         }
 //         System.out.println(count);
 //     }
 // }
-
-
-
 
 ///////////// Problem -> 4
 // Given an unsorted array with both pos & neg 
@@ -468,5 +450,437 @@ import java.util.*;
 //             }
 //             System.out.println(min);
 //         }
+//     }
+// }
+
+
+
+
+// Date: 05/05/2026
+///////////// Problem -> 1
+// Given an array, the task is to find number of subarrays with sum at least K.
+// Input
+// I/P : 6 1 2
+// K = 10
+// Output
+// O/P : 0
+// Input
+// I/P : 3 3 3
+// K = 5
+// Output
+// O/P : 3
+
+// public class problem {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+//         for(int i = 0; i < n; i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         int k = sc.nextInt();
+
+//         int count = 0;
+//         for(int i = 0; i < n; i++){
+//             int sum = 0;
+//             int maxof = 0;
+//             for(int j = i; j < n; j++){
+//                 sum = Math.max(arr[j], sum + arr[j]);
+//                 maxof = Math.max(sum, maxof);
+//                 if(sum >= k){
+//                     count++;
+//                 }
+//             }
+//         }
+//         System.out.println("O/P: "+count);
+//     }
+// }
+
+
+
+
+///////////// Problem -> 2
+// Given an array, the task is to calculate the sum of all subarrays of size K.
+// Input
+// I/P : 1 2 3 4 5 6
+// K = 3
+// Output
+// O/P : 6 9 12 15
+// Input
+// I/P : 1 -2 3 4 -5 6
+// K = 2
+// Output
+// O/P : -1 1 7 -1 1
+
+// public class problem {
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+
+//         for(int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         int k = sc.nextInt();
+
+//         for(int i = 0; i <= n - k; i++) {
+
+//             int[] arr2 = new int[k];
+
+//             int sum = 0;
+//             int x = 0;
+
+//             for(int j = i; j < n; j++) {
+
+//                 if(x == k) {
+//                     break;
+//                 }
+
+//                 arr2[x] = arr[j];
+//                 x++;
+//             }
+
+//             for(int l = 0; l < k; l++) {
+//                 sum += arr2[l];
+//             }
+
+//             System.out.print(sum + " ");
+//         }
+//     }
+// }
+
+
+
+
+///////////// Problem -> 3
+// Given an array, the task is to split the array into 2 subarrays 
+// such that difference between their maximum elements is minimum.
+// Input
+// I/P : 7 9 5 10
+// Output
+// O/P : 1
+// Input
+// I/P : 6 6 6
+// Output
+// O/P : 0
+
+// public class problem {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         int min = Integer.MAX_VALUE;
+//         for(int i = 0; i < n; i++){
+//             int max1 = 0;
+//             for(int k = 0; k < i; k++){
+//                 if(arr[k] > max1){
+//                     max1 = arr[k];
+//                 }
+//             }
+//             int max2 = 0;
+//             for(int j = i; j < n; j++){
+//                 if(arr[j] > max2){
+//                     max2 = arr[i];
+//                 }
+//             }
+//             int temp = 0;
+//             if(max1 >= max2){
+//                 temp = max1 - max2;
+//             }else{
+//                 temp = max2 - max1;
+//             }
+
+//             if(temp < min){
+//                 min = temp;
+//             }
+//         }
+//         System.out.println(min);
+//     }
+// }
+
+
+
+
+///////////// Problem -> 4
+// Given an unsorted array, the task is to count number of subarrays 
+// with exactly K perfect square numbers.
+// Input
+// I/P : 2 4 9 3
+// K = 2
+// Output
+// O/P : 4
+// Input
+// I/P : 4 2 5
+// K = 3
+// Output
+// O/P : 0
+
+// public class problem {
+//     static boolean check(int n){
+//         boolean find = false;
+
+//         for(int i = 1; i * i <= n; i++){
+//             if(i * i == n){
+//                 find = true;
+//                 break;
+//             }
+//         }
+//         return find;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+//         for(int i = 0; i < n; i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         int k = sc.nextInt();
+
+//         int total = 0;
+//         for(int i = 0; i < n; i++){
+//             for(int j = i; j < n; j++){
+//                 int count = 0;
+//                 for(int x = i; x < j + 1; x++){
+//                     if(check(arr[x])){
+//                         count++;
+//                     }
+//                 }
+//                 if(count == k){
+//                     total++;
+//                 }
+//             }
+//         }
+//         System.out.println("total: "+total);
+//     }
+// }
+
+
+
+
+///////////// Problem -> 5
+// Given an array, the task is to find length of longest alternating even odd subarray.
+// Input
+// I/P : 1 2 3 4 5 7 9
+// Output
+// O/P : 5
+// Input
+// I/P : 1 3 5
+// Output
+// O/P : 0
+
+// public class problem {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         boolean change = true;
+//         int count = 0;
+//         int max = Integer.MIN_VALUE;
+//         for(int i = 0; i < n; i++){
+//             if(arr[i] % 2 == 0){
+//                 change = true;
+//                 count = 1;
+//             }else{
+//                 change = false;
+//                 count = 1;
+//             }
+//             for(int j = i + 1; j < n; j++){
+//                 if(arr[j] % 2 == 0 && change == false){
+//                     count++;
+//                 }
+//                 else if(arr[j] % 2 != 0 && change == true){
+//                     count++;
+//                 }
+//             }
+//             if(count == 1){
+//                 count = 0;
+//             }
+//             max = Math.max(max, count);
+//         }
+
+//         System.out.println(max);
+
+//     }
+// }
+
+
+
+
+// Date: 09/05/2026
+///////////// Problem -> 1
+// Given an array, the task is to find total count of subarrays such 
+// that sum of elements at even positions and sum of elements at odd positions are equal.
+// Input
+// I/P : 1 2 3 4
+// Output
+// O/P : 0
+// Input
+// I/P : 2 4 6 4 2
+// Output
+// O/P : 2
+
+// public class problem {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         int count = 0;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i; j < n; j++) {
+//                 int ind = 1;
+//                 int even = 0;
+//                 int odd = 0;
+//                 for (int k = i; k < j + 1; k++) {
+//                     if (ind % 2 != 0) {
+//                         odd += arr[k];
+//                         ind++;
+//                     } else {
+//                         even += arr[k];
+//                         ind++;
+//                     }
+//                 }
+//                 if (odd == even) {
+//                     count++;
+//                 }
+//             }
+//         }
+
+//         System.out.println(count);
+
+//     }
+// }
+
+
+
+
+///////////// Problem -> 2
+// Given an array, the task is to split the array into 2 subarrays 
+// such that difference between their sums is minimum.
+// Input
+// I/P : 7 9 5 10
+// Output
+// O/P : 1
+// Input
+// I/P : 6 6 6
+// Output
+// O/P : 6
+
+// public class problem {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         int min = Integer.MAX_VALUE;
+//         for (int i = 0; i < n; i++) {
+//             int sum1 = 0;
+//             int sum2 = 0;
+//             int diff = 0;
+//             for (int j = 0; j < i; j++) {
+//                 sum1 += arr[j];
+//             }
+//             for (int k = i; k < n; k++) {
+//                 sum2 += arr[k];
+//             }
+
+//             if (sum1 >= sum2) {
+//                 diff = sum1 - sum2;
+//             } else {
+//                 diff = sum2 - sum1;
+//             }
+
+//             if (diff < min) {
+//                 min = diff;
+//             }
+//         }
+//         System.out.println(min);
+//     }
+// }
+
+
+
+
+///////////// Problem -> 3
+// Given an array, the task is to print maximum number of non-overlapping 
+// subarrays with sum equal to K.
+// Input
+// I/P : -2 6 6 3 5 4 1 2 8
+// K = 10
+// Output
+// O/P : 3
+// Input
+// I/P : 1 1 1 1
+// K = 2
+// Output
+// O/P : 2
+
+// public class problem {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         int k = sc.nextInt();
+//         int count = 0;
+//         int i = 0;
+//         int ind = 0;
+//         while(i < n) {
+//             int sum = 0;
+//             ind = i + 1;
+//             while(true){
+//                 if(i < n){
+//                     sum += arr[i];
+//                     if (sum == k) {
+//                         count++;
+//                         i++;
+//                         break;
+//                     } else if (sum > k) {
+//                         i = ind;
+//                         break;
+//                     }
+//                     i++;
+//                 }
+//                 else{
+//                     break;
+//                 }
+//             }
+//         }
+//         System.out.println(count);
 //     }
 // }
