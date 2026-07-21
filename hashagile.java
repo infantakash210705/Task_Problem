0a6fc687b75c4114d48ad22c62ffaf1c5545e9ac
INFANT AKASH W <infantakash260@example.com>
2026-07-21 16:12:58 +0530

    this is a latest commit

--- hashagile.java
+++ hashagile.java
@@ -1,7 +1,7 @@
 public class hashagile {
     public static void main(String[] args) {
         String str = "PROGRAM";
-        int n = str.length();
+        // int n = str.length();
         // for(int i = 0; i < str.length(); i++){
         //     for(int j = 0; j < str.length(); j++){
         //         // if(i == j || j == str.length() - 1 - i){
@@ -24,22 +24,39 @@ public class hashagile {
         //     System.out.println();
         // }
 
-        for (int i = 0; i < n; i++) {
-            System.out.print(str.charAt(i) + " ");
-        }
-        System.out.println();
+        // for (int i = 0; i < n; i++) {
+        //     System.out.print(str.charAt(i) + " ");
+        // }
+        // System.out.println();
 
-        for (int i = 0; i < n - 2; i++) {
+        // for (int i = 0; i < n-1; i++) {
 
-            for (int j = 0; j < (n - i - 2) * 2; j++) {
-                System.out.print(" ");
+        //     for (int j = 0; j < n - i - 1; j++) {
+        //         System.out.print("  ");
+        //     }
+
+        //     System.out.println(str.charAt(i));
+        // }
+
+        // for (int i = n - 1; i >= 0; i--) {
+        //     System.out.print(str.charAt(i) + " ");
+        // }
+
+        int n = 4;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n - i; j++){
+                System.out.print("  ");
             }
 
-            System.out.println(str.charAt(i));
-        }
+            char ch = 'A';
 
-        for (int i = n - 1; i >= 0; i--) {
-            System.out.print(str.charAt(i) + " ");
+            for(int j = 1; j <= 2 * i - 2; j++){
+                System.out.print(ch+" ");
+                ch++;
+            }
+
+            System.out.print('A');
+            System.out.println();
         }
     }
 }
---
+++ zoho.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class zoho {
+    public static void main(String[] args) {
+        /*
+        String str = "PROGRAM";
+        int n = str.length();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == i || j == n - i - 1)
+                    System.out.print(str.charAt(j));
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        */
+
+        int arr[]={10,36,54,89,12};
+
+        Arrays.sort(arr);
+
+        for(int x:arr)
+        {
+            System.out.println("<"+x+","+weight(x)+">");
+        }
+    }
+
+    static int weight(int n) {
+
+        int w = 0;
+
+        int root = (int)Math.sqrt(n);
+
+        if(root*root==n)
+            w+=5;
+
+        if(n%4==0 && n%6==0)
+            w+=4;
+
+        if(n%2==0)
+            w+=3;
+
+        return w;
+    }
+    
+}
