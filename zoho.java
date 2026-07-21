483f46760bc98d64748c04c0008610b095223445
INFANT AKASH W <infantakash260@example.com>
2026-07-21 16:24:29 +0530

    latest updated

--- zoho.java
+++ zoho.java
@@ -1,8 +1,7 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class zoho {
     public static void main(String[] args) {
-        /*
         String str = "PROGRAM";
         int n = str.length();
 
@@ -15,34 +14,32 @@ public class zoho {
             }
             System.out.println();
         }
-        */
 
-        int arr[]={10,36,54,89,12};
+        // int arr[]={10,36,54,89,12};
 
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
 
-        for(int x:arr)
-        {
-            System.out.println("<"+x+","+weight(x)+">");
-        }
+        // for(int x:arr)
+        // {
+        //     System.out.println("<"+x+","+weight(x)+">");
+        // }
     }
 
-    static int weight(int n) {
+    // static int weight(int n) {
 
-        int w = 0;
+    //     int w = 0;
 
-        int root = (int)Math.sqrt(n);
+    //     int root = (int)Math.sqrt(n);
 
-        if(root*root==n)
-            w+=5;
+    //     if(root*root==n)
+    //         w+=5;
 
-        if(n%4==0 && n%6==0)
-            w+=4;
+    //     if(n%4==0 && n%6==0)
+    //         w+=4;
 
-        if(n%2==0)
-            w+=3;
+    //     if(n%2==0)
+    //         w+=3;
 
-        return w;
-    }
-    
+    //     return w;
+    // }
 }
